38895273df2670338a31f3d2f262d959e98d475a
anne <anne.thiess@students.fhnw.ch>
2023-01-20 09:45:39 +0100

    dckjdndvnlvd

--- Lab1/index.php
+++ Lab1/index.php
@@ -1,18 +1,16 @@
 <?php 
-
-
+$filename
 
 ?>
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lab1</title>
+    <script src="Lab1.js"></script>
+    <link rel="stylesheet" href="Lab1.css">
+    <title>Lab1</title>+
 </head>
 <body>
 <div class="grid-container">

a55e222363795137b7753f9478b787371beef47e
anne <anne.thiess@students.fhnw.ch>
2023-01-20 10:29:49 +0100

    Commit

--- Lab1/index.php
+++ Lab1/index.php
@@ -1,7 +1,27 @@
-<?php 
-$filename
+<?php
 
+$servername = "localhost";
+$username = "root";
+$password = "root";
+
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+
+
+// Check connection
+if ($conn->connect_error) {
+
+  die("Connection failed: " . $conn->connect_error);
+
+}
+
+echo "Connected successfully";
 ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>

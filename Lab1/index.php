0bb5df887bc4dd2f616a7f1c86aa4bab1d10a40f
anne <anne.thiess@students.fhnw.ch>
2023-01-20 09:35:59 +0100

    Commit 2

---
+++ Lab1/index.php
@@ -0,0 +1,31 @@
+<?php 
+
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab1</title>
+</head>
+<body>
+<div class="grid-container">
+  <div class="grid-item">1</div>
+  <div class="grid-item">2</div>
+  <div class="grid-item">3</div>
+  <div class="grid-item">4</div>
+  <div class="grid-item">5</div>
+  <div class="grid-item">6</div>
+  <div class="grid-item">7</div>
+  <div class="grid-item">8</div>
+  <div class="grid-item">9</div>
+</div>
+    <h1>Lab Assignment 1</h1>
+</body>
+</html>

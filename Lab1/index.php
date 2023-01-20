ccd437db884498ddde06efd9b407f8cc61aef3c8
anne <anne.thiess@students.fhnw.ch>
2023-01-20 11:31:08 +0100

    commitvfvf

--- Lab1/index.php
+++ Lab1/index.php
@@ -3,10 +3,10 @@
 $servername = "localhost";
 $username = "root";
 $password = "root";
-
+$dbname = "todo_application";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 
 // Check connection
@@ -14,31 +14,40 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
+
+
+$sql = "SELECT id FROM tasks";
+$result = $conn->query($sql);
+
+if ($result -> num_rows > 0) {
+  // output data of each row
+  while ($row = mysqli_fetch_assoc($result)) {
+    echo "id: " . $row["id"];
+    echo "<p> id: ". $row["id"]. " description: ". $row["description"]. "</p>";
+  }
+} else {
+  echo "0 to dos results";
+}
+
+mysqli_close($conn);
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="lab1.js"></script>
-    <link rel="stylesheet" href="lab1.css">
-    <title>Lab1</title>+
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="lab1.js"></script>
+  <link rel="stylesheet" href="lab1.css">
+  <title>Lab1</title>+
 </head>
+
 <body>
-<h1>Lab Assignment 1</h1>
-<div class="grid-container">
-  <div class="grid-item">Lab1</div>
-  <div class="grid-item">2</div>
-  <div class="grid-item">3</div>
-  <div class="grid-item">4</div>
-  <div class="grid-item">5</div>
-  <div class="grid-item">6</div>
-  <div class="grid-item">7</div>
-  <div class="grid-item">8</div>
-  <div class="grid-item">9</div>
-</div>
+  <h1>Lab Assignment 1</h1>
+
 </body>
+
 </html>

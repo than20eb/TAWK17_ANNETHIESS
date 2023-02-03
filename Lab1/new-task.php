8994d62ae75656bb60b262c88b5f4d4b8701edca
anne <anne.thiess@students.fhnw.ch>
2023-02-03 11:44:53 +0100

    Commited

--- Lab1/new-task.php
+++ Lab1/new-task.php
@@ -1,4 +1,21 @@
-<?php
 
 
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>New car</title>
+    <script src="script.js"></script>
+</head>
+<body>
+    <h1>New car</h1>
+    
+    <form action="create-car.php" method="post">
+        <input type="text" name="make" placeholder="Make"> <br>
+        <input type="text" name="model" placeholder="Model"> <br>
+        <input type="submit" value="Save">
+    </form>
+</body>
+</html>

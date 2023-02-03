c91f74c9a558a38a62fb9c36d3a84e0b479ae20e
Anne Thieß <anne.thiess12@gmail.com>
2023-02-03 19:46:33 +0100

    kllkkl

--- Lab1/new-task.php
+++ Lab1/new-task.php
@@ -17,14 +17,14 @@
         <h2>Create a new task</h2>
 
         <form action="create-task.php" method="post">
-            <label>Task:</label><br>
-            <input type="text" name="title" value=""> <br>
+            <p>Task:</p> <input type="text" name="title" value=""> <br>
 
-            <input type="text" name="description" value=""> <br>
+            <p>Description:</p> <input type="text" name="description" value=""> <br>
 
             <input type="submit" value="Save">
         </form>
     </div>
+
     <script src="script.js"></script>
 </body>
 

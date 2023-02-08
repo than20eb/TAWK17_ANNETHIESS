3e6f73a8ea50bd95f815878b21a38e1305b9597e
Anne Thieß <anne.thiess12@gmail.com>
2023-02-08 16:10:53 +0100

    s
    s

--- Lab1/update-task.php
+++ Lab1/update-task.php
@@ -1,11 +1,16 @@
 <?php
-// Create connection to Database
-$conn = new mysqli("localhost", "todo_application", "", "tasks");
-// Check up: If connection to Database is missing
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "todo_application";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-
-
+}
 $title = $_POST["title"];
 $description = $_POST["description"];
 $status = $_POST["status"];
